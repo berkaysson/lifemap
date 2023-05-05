226909a6e667f4335ca43bd85a2f65f1e1c41d39
berkaysson <berkaysonel85@gmail.com>
2023-05-05 20:19:33 +0300

    Implement IndexedDB in index.js and pass to App.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const STORES = ['2020', '2021', '2022', '2023'];
+const DB_VERSION = 1;
+const DB_NAME = 'lifemap';
+
+// Open the database
+const openDB = () => {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open(DB_NAME, DB_VERSION);
+
+    // Handle errors
+    request.onerror = () => {
+      console.error('Database error:', request.error);
+      reject(request.error);
+    };
+
+    // Handle success
+    request.onsuccess = () => {
+      const db = request.result;
+      console.log('Database opened successfully:', db);
+      resolve(db);
+    };
+
+    // Handle upgrades
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      console.log('Database upgrade needed:', db);
+
+      // Create object stores for each year
+      STORES.forEach((storeName) => {
+        const store = db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+
+        // Define the store schema here
+      });
+    };
+  });
+};
+
+// Render the app inside a root element
+const rootElement = document.getElementById('root');
+
+openDB().then((db) => {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <App db={db} />
+    </React.StrictMode>
+  );
+}).catch((error) => {
+  console.error('Failed to open database:', error);
+});
