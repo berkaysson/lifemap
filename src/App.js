9610b9e5476d71da5046c570303c9865d546d69c
berkaysson <berkaysonel85@gmail.com>
2023-05-05 20:20:55 +0300

    App.js and some methods to handle database changes added, also create AppContent.js to handle tree of components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,94 @@ import { useEffect, useState } from "react";
 import dataUnitConstructor from "./Data/dataUnitConstructor";
 import AppContent from "./Components/AppContent";
 
+const CURRENT_STORE = "2023"
+
 function App({db}) {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const createDataUnit = () => {
+    const dataUnitDate = (new Date().toISOString()).slice(0,10);
+    const dataUnit = dataUnitConstructor(dataUnitDate);
+
+    const transaction = db.transaction([CURRENT_STORE], 'readwrite');
+    const dataStore = transaction.objectStore(CURRENT_STORE);
+
+    const request = dataStore.add(dataUnit);
+
+    request.onsuccess = () => {
+      console.log('Data unit added successfully');
+    };
+
+    request.onerror = (event) => {
+      console.error('Error adding data unit:', event.target.error);
+    };
+  }
+
+  const getDataUnit = (dateID) => {
+    return new Promise((resolve, reject) => {
+      const selectedYear = dateID.slice(0,4).toString();
+  
+      const transaction = db.transaction([selectedYear], 'readonly');
+      const dataStore = transaction.objectStore(selectedYear);
+  
+      const request = dataStore.get(dateID);
+  
+      request.onsuccess = (event) => {
+        const dataUnit = event.target.result;
+        if (dataUnit) {
+          console.log('Data unit retrieved successfully');
+          resolve(dataUnit);
+        } else {
+          console.error('Data unit not found');
+          resolve(null);
+        }
+      }
+  
+      request.onerror = (event) => {
+        console.error('Error getting data unit:', event.target.error);
+        reject(event.target.error);
+      }
+    });
+  }
+
+  const getDataUnitbyDate = (date) => {
+    getDataUnit(date)
+    .then((dataUnit) => {
+      setSelectedDate(dataUnit);
+      console.log('Data unit assigned successfully');
+    })
+    .catch((error) => {
+      console.error('Failed to get data unit:', error);
+    });
+  }
+
+  const updateDataUnit = (toBeUpdatedData) => {
+    getDataUnit(toBeUpdatedData.date)
+    .then((dataUnit) => {
+      console.log('Data to be updated retrieved successfully')
+      const selectedYear = toBeUpdatedData.date.slice(0,4).toString();
+      dataUnit[toBeUpdatedData.category][toBeUpdatedData.subCategory] = toBeUpdatedData.value;
+
+      const transaction = db.transaction([selectedYear], "readwrite");
+      const dataStore = transaction.objectStore(selectedYear);
+      const request = dataStore.put(dataUnit);
+
+      request.onsuccess = () => {
+        console.log('Data unit updated successfully');
+      };
+      
+      request.onerror = (event) => {
+        console.error('Error updating data unit:', event.target.error);
+      };
+    })
+    .catch((error) => {
+      console.error('Failed to get data unit:', error);
+    })
+  }
   
   return (
     <div className="App">
-      <AppContent />
+      <AppContent onCreateToday={createDataUnit} onGetDataByDate={getDataUnitbyDate} onUpdateData={updateDataUnit} selectedDate={selectedDate} />
     </div>
   );
 }
