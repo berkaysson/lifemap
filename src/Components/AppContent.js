89cbec3d2cc337d82a4dddcb77608a5420bbbd89
berkaysson <berkaysonel85@gmail.com>
2023-05-06 21:35:17 +0300

    Create a CategoryUpdaterForm to adding and deleting a sub category to category

--- src/Components/AppContent.js
+++ src/Components/AppContent.js
@@ -1,16 +1,30 @@
 import DataViewer from "./DataViewer";
 import DataViewerForm from "./Forms/DataViewerForm";
 import DataUpdaterForm from "./Forms/DataUpdaterForm";
+import CategoryUpdateForm from "./Forms/CategoryUpdateForm";
 
-const AppContent = ({onCreateToday, onGetDataByDate, onUpdateData, selectedDate}) => {
+const AppContent = ({
+  onCreateToday,
+  onGetDataByDate,
+  onUpdateData,
+  selectedDate,
+  onGetAllCategories,
+  onUpdateCategory,
+  onDeleteSubCategory
+}) => {
   return (
     <>
       <button onClick={onCreateToday}>Create Today</button>
       <DataUpdaterForm onUpdateData={onUpdateData} />
       <DataViewerForm onDateSelection={onGetDataByDate} />
+      <CategoryUpdateForm
+        onGetAllCategories={onGetAllCategories}
+        onUpdateCategory={onUpdateCategory}
+        onDeleteSubCategory={onDeleteSubCategory}
+      />
       <DataViewer selectedDate={selectedDate} />
     </>
   );
-}
+};
 
 export default AppContent;
