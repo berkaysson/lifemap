ac5e416b9dd3272c48a72a30c184dfd6e652b864
berkaysson <berkaysonel85@gmail.com>
2023-05-08 19:09:11 +0300

    Implement react-select Select and SubCategorySelect components

--- src/Components/Forms/DataUpdaterForm.js
+++ src/Components/Forms/DataUpdaterForm.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
+import Select from "react-select";
+import SubCategorySelect from "./SubCategorySelect";
+
 import { useEffect, useState } from "react";
 
 const DataUpdaterFormWrapper = styled.div`
@@ -9,11 +12,25 @@ const DataUpdaterFormWrapper = styled.div`
 const DataUpdaterForm = ({onUpdateData, onGetAllCategories}) => {
   const selectedDate = (new Date().toISOString()).slice(0,10);
   const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('Study');
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const [subCategories, setSubCategories] = useState([]);
   const [selectedSubCategory, setselectedSubCategory] = useState(null);
 
   let toBeUpdatedData = {};
+
+  const categoryOptions = categories.map((category) => ({
+    label: category.name,
+    value: category.name,
+    category: category
+  }));
+
+  const subCategoryOptions = subCategories.map((subCategory) => {
+    return {
+      label:subCategory,
+      value:subCategory,
+      subCategory:subCategory
+    }
+  })
 
   useEffect(()=>{
     async function fetchCategories(){
@@ -28,22 +45,22 @@ const DataUpdaterForm = ({onUpdateData, onGetAllCategories}) => {
     e.preventDefault();
     toBeUpdatedData = {
       date:selectedDate,
-      category: selectedCategory,
-      subCategory: selectedSubCategory,
+      category: selectedCategory.value,
+      subCategory: selectedSubCategory.value,
       value:e.target.valueInput.value
     }
     onUpdateData(toBeUpdatedData);
   }
 
-  const categorySelectionHandler = (event) => {
-    setSelectedCategory(event.target.value);
+  const categorySelectionHandler = (selectedCategory) => {
+    setSelectedCategory(selectedCategory);
 
-    const category = categories.find((item) => item.name === event.target.value);
+    const category = categories.find((item) => item.name === selectedCategory.value);
     setSubCategories(category.subCategories)
   }
 
-  const subCategorySelectHandler = (event) => {
-    setselectedSubCategory(event.target.value);
+  const subCategorySelectHandler = (selectedSubCategory) => {
+    setselectedSubCategory(selectedSubCategory);
   }
 
   return (
@@ -51,27 +68,18 @@ const DataUpdaterForm = ({onUpdateData, onGetAllCategories}) => {
       <h4>Current Date: {selectedDate}</h4>
       <form onSubmit={submitHandler}>
         <label>Select Updated Category</label>
-        <select onChange={categorySelectionHandler}>
-          <option disabled>--Select a Category--</option>
-          {categories.map((item, index) => (
-            <option key={index} value={item.name}>
-              {item.name}
-            </option>
-          ))}
-        </select>
+        <Select
+          onChange={categorySelectionHandler}
+          options={categoryOptions}
+          placeholder="--Select a category--"
+          value={selectedCategory}
+        />
         <label>Select SubCategory</label>
-          {subCategories && subCategories.length > 0 ? (
-            <>
-              <select onChange={subCategorySelectHandler}>
-                <option disabled>Select a subCategory</option>
-                {subCategories.map((item) => (
-                  <option value={item}>{item}</option>
-                ))}
-              </select>
-            </>
-          ) : (
-            <p>No subCategories found</p>
-          )}
+          <SubCategorySelect 
+            placeholder={"--Select a subCategory--"}
+            onChange={subCategorySelectHandler}
+            options={subCategoryOptions}
+            />
         <label>Enter the value</label>
         <input type="number" name="valueInput"></input>
         <button type="submit">Submit</button>
