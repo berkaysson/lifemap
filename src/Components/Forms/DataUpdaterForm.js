3a07a9a0e82a885d3eefb099d9992319cba19788
berkaysson <berkaysonel85@gmail.com>
2023-05-10 18:13:19 +0300

    Comments and format document

--- src/Components/Forms/DataUpdaterForm.js
+++ src/Components/Forms/DataUpdaterForm.js
@@ -7,18 +7,20 @@ import { useEffect, useState } from "react";
 
 const DataUpdaterFormWrapper = styled.div`
   border: 2px red solid;
-`
+`;
 
 const DataUpdaterForm = ({ onUpdateData, categories, categoryOptions }) => {
   const selectedDate = new Date().toISOString().slice(0, 10);
+
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [subCategories, setSubCategories] = useState([]);
-  const [selectedSubCategory, setselectedSubCategory] = useState(null);
+  const [selectedSubCategory, setSelectedSubCategory] = useState(null);
 
   const [subCategoryOptions, setSubCategoryOptions] = useState();
 
   let toBeUpdatedData = {};
 
+  // Fetch the list of categories on mount and update the select dropdown options
   async function fetchCategories() {
     const selectedCategoryName = selectedCategory?.value;
     const category = categories.find(
@@ -27,6 +29,12 @@ const DataUpdaterForm = ({ onUpdateData, categories, categoryOptions }) => {
     setSubCategories(category?.subCategories ?? []);
   }
 
+  useEffect(() => {
+    fetchCategories();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Update the select dropdown options when the subCategories change  
   useEffect(() => {
     setSubCategoryOptions(() =>
       subCategories.map((subCategory) => ({
@@ -37,11 +45,7 @@ const DataUpdaterForm = ({ onUpdateData, categories, categoryOptions }) => {
     );
   }, [categories, subCategories]);
 
-  useEffect(() => {
-    fetchCategories();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
+  // Handle form submission to update data with toBeUpdatedData object
   const submitHandler = async (e) => {
     e.preventDefault();
     toBeUpdatedData = {
@@ -53,9 +57,10 @@ const DataUpdaterForm = ({ onUpdateData, categories, categoryOptions }) => {
     onUpdateData(toBeUpdatedData);
   };
 
+  // Handle selection of a category from the dropdown
   const categorySelectionHandler = (selectedCategory) => {
     setSelectedCategory(selectedCategory);
-    setselectedSubCategory(null);
+    setSelectedSubCategory(null);
 
     const category = categories.find(
       (item) => item.name === selectedCategory.value
@@ -63,8 +68,9 @@ const DataUpdaterForm = ({ onUpdateData, categories, categoryOptions }) => {
     setSubCategories(category.subCategories);
   };
 
+  // Handle selection of a subcategory from the dropdown  
   const subCategorySelectHandler = (selectedSubCategory) => {
-    setselectedSubCategory(selectedSubCategory);
+    setSelectedSubCategory(selectedSubCategory);
   };
 
   return (
