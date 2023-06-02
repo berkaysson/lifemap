14ba1f7783c2c9d5b0f0c7fdc4be5bfd569d0e8d
berkaysson <berkaysonel85@gmail.com>
2023-06-02 21:02:41 +0300

    Add color identifier for fulfilled prop of tasks and habits

--- src/Components/HabitItem.js
+++ src/Components/HabitItem.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const HabitItem = ({ habit, onDeleteHabitDataUnit }) => {
   const deleteHandler = () => {
-    onDeleteHabitDataUnit(habit.id)
-  }
+    onDeleteHabitDataUnit(habit.id);
+  };
   return (
     <li id={habit?.id}>
-      <h3>{habit?.nameValue}</h3>
+      <h3 style={{ color: habit?.fulfilled ? "green" : "red" }}>
+        {habit?.nameValue}
+      </h3>
       <p>Category: {habit?.category.label}</p>
       <p>Subcategory: {habit?.subCategory.label}</p>
       <p>Start Date: {habit?.startDate}</p>
       <p>End Date: {habit?.endDate}</p>
       <p>Time Value: {habit?.timeValue}</p>
       <p>Check Frequency: {habit?.frequency}</p>
-      <button type="button" onClick={deleteHandler}>Delete</button>
+      <button type="button" onClick={deleteHandler}>
+        Delete
+      </button>
     </li>
   );
 };
--- src/Components/TaskItem.js
+++ src/Components/TaskItem.js
@@ -2,18 +2,22 @@ import React from "react";
 
 const TaskItem = ({ task, onDeleteTaskDataUnit }) => {
   const deleteHandler = () => {
-    onDeleteTaskDataUnit(task.id)
-  }
-
+    onDeleteTaskDataUnit(task.id);
+  };
+  console.log(task);
   return (
     <li id={task?.id}>
-      <h3>{task?.nameValue}</h3>
+      <h3 style={{ color: task?.fulfilled ? "green" : "red" }}>
+        {task?.nameValue}
+      </h3>
       <p>Category: {task?.category.label}</p>
       <p>Subcategory: {task?.subCategory.label}</p>
       <p>Start Date: {task?.startDate}</p>
       <p>End Date: {task?.endDate}</p>
       <p>Time Value: {task?.timeValue}</p>
-      <button type="button" onClick={deleteHandler}>Delete</button>
+      <button type="button" onClick={deleteHandler}>
+        Delete
+      </button>
     </li>
   );
 };
