408954aea7cda91f62711733bcb2500183e6df34
berkaysson <berkaysonel85@gmail.com>
2023-06-03 16:53:41 +0300

    Add calculateCurrentTimeValue, checkDueDate and checkIsFulfilled functions

--- src/Utilities/tasksAndHabits.js
+++ src/Utilities/tasksAndHabits.js
@@ -1,10 +1,13 @@
+import { addDays } from "date-fns";
+import moment from "moment";
+
 export const addTaskOrHabitDataUnit = async (db, unit, dataType) => {
   try {
     if (dataType === "task") {
-      await db.tasksData.put({ ...unit, fulfilled: false, isClosed: false });
+      await db.tasksData.put({ ...unit, isFulfilled: false, isClosed: false });
       console.log("Task unit added successfully");
     } else if (dataType === "habit") {
-      await db.habitsData.put({ ...unit, fulfilled: false, isClosed: false });
+      await db.habitsData.put({ ...unit, isFulfilled: false, isClosed: false });
       console.log("Habit unit added successfully");
     }
   } catch (error) {
@@ -45,4 +48,37 @@ export const getAllTaskOrHabitDataUnits = async (db, dataType) => {
   } catch (error) {
     console.error("Error getting all data units:", error);
   }
+};
+
+const calculateCurrentTimeValue = (taskUnit, activityDataUnits) => {
+  const category = taskUnit.category.label;
+  const subCategory = taskUnit.subCategory.value;
+  let currentTimeValue = 0;
+
+  for (
+    let startDate = moment(taskUnit.startDate);
+    startDate <= moment(taskUnit.endDate);
+    startDate.add(1, "days")
+  ) {
+    const date = startDate.format("YYYY-MM-DD");
+    const dataUnit = activityDataUnits.find(
+      (dataUnit) => dataUnit.date === date
+    );
+    if (dataUnit) {
+      currentTimeValue += dataUnit[category][subCategory] * 1;
+    }
+  }
+
+  return currentTimeValue;
+};
+
+export const checkDueDate = (unit) => {
+  const today = moment(addDays(new Date(), -1));
+  const dueDate = moment(new Date(unit.endDate));
+  return dueDate.isBefore(today);
+};
+
+export const checkIsFulfilled = (taskUnit, activityDataUnits) => {
+  let currentTimeValue = calculateCurrentTimeValue(taskUnit, activityDataUnits);
+  return currentTimeValue >= taskUnit.timeValue;
 };
