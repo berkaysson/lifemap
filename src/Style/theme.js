c61dccac02bacfffad226517d1dfdfc68509aa56
berkaysson <berkaysonel85@gmail.com>
2023-06-07 22:19:23 +0300

    Create theme variables

---
+++ src/Style/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primary: "#0000001",
+    secondary: "#FFFFFF",
+    alternative: "#2b2d42",
+    theme: "#fca311",
+  },
+  sizes: {
+    small: "8px",
+    medium: "16px",
+    large: "24px",
+  }
+}
