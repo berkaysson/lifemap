7e33f57e06224223847d0a96f5e304ffdb205c19
berkaysson <berkaysonel85@gmail.com>
2023-06-07 22:01:34 +0300

    Main layout style

--- src/Layout/RootLayout.js
+++ src/Layout/RootLayout.js
@@ -1,18 +1,37 @@
 import NavBar from "./NavBar";
 
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const RootContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  min-height: 100vh;
+`;
+
+const Sidebar = styled.aside`
+  background-color: #f5f5f5;
+`;
+
+const MainContent = styled.main`
+  padding: 20px;
+`;
+
+const NavBarWrapper = styled(NavBar)`
+  width: 100%;
+`;
 
 const RootLayout = () => {
   return (
-    <div>
-      <header>
-        <NavBar />
-      </header>
+    <RootContainer>
+      <Sidebar>
+        <NavBarWrapper />
+      </Sidebar>
 
-      <main>
+      <MainContent>
         <Outlet />
-      </main>
-    </div>
+      </MainContent>
+    </RootContainer>
   );
 };
 
