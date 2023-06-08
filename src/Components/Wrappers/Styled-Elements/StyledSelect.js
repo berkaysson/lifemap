ddbc45bda6fe3bd5ce63263f95b0915d42d4ffe7
berkaysson <berkaysonel85@gmail.com>
2023-06-09 02:08:23 +0300

    Create StyledSelect using react-select and styled-components

---
+++ src/Components/Wrappers/Styled-Elements/StyledSelect.js
@@ -0,0 +1,29 @@
+import Select from "react-select";
+import styled from "styled-components";
+
+import {theme as myTheme} from "../../../Style/theme";
+
+const StyledSelectWrapper = styled(Select)`
+  box-shadow: ${myTheme.boxShadows.smallCardShadow};
+  border-radius: ${myTheme.radius.small};
+  font-weight: bold;
+  max-width: 300px;
+`
+
+export const StyledSelect = (props) => {
+  return (
+    <StyledSelectWrapper 
+    {...props}
+    theme={(theme)=>({
+      ...theme,
+      borderRadius:`${myTheme.radius.small}`,
+      colors: {
+        ...theme.colors,
+        primary25: `${myTheme.colors.alternative}`,
+        primary50: `${myTheme.colors.theme}`,
+        primary: `${myTheme.colors.theme}`,
+      },
+    })}
+    />
+  )
+}
