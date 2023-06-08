2cbeeff20d323859036bdb9d6da92d526cee258a
berkaysson <berkaysonel85@gmail.com>
2023-06-08 19:09:36 +0300

    Create boxShadows

--- src/Style/theme.js
+++ src/Style/theme.js
@@ -1,13 +1,21 @@
+const themeColors = "#fca311"
+
 export const theme = {
   colors: {
     primary: "#000000",
     secondary: "#FFFFFF",
     alternative: "#2b2d42",
-    theme: "#fca311",
+    theme: themeColors,
   },
   sizes: {
     small: "8px",
     medium: "16px",
     large: "24px",
+  },
+  radius: {
+    medium: "20px",
+  },
+  boxShadows: {
+    themeShadow: `0 0 6px 0px ${themeColors}`,
   }
 }
