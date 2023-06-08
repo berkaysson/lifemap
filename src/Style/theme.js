ba7b405b45f33e36a7648d11a2909bfb332c382a
berkaysson <berkaysonel85@gmail.com>
2023-06-08 22:06:45 +0300

    Change alternative and theme color, add smallCardShadow to boxShadows

--- src/Style/theme.js
+++ src/Style/theme.js
@@ -1,10 +1,10 @@
-const themeColors = "#fca311";
+const themeColors = "#abc4ff";
 
 export const theme = {
   colors: {
     primary: "#000000",
     secondary: "#FFFFFF",
-    alternative: "#2b2d42",
+    alternative: "#d3d3d3",
     theme: themeColors,
   },
   sizes: {
@@ -19,8 +19,9 @@ export const theme = {
   },
   boxShadows: {
     themeShadow: `0 0 6px 0px ${themeColors}`,
-    largeCardShadow:
-      "rgba(0, 0, 0, 0.5) 0px 20px 20px 5px",
+    largeCardShadow: "rgba(0, 0, 0, 0.5) 0px 20px 20px 5px",
+    smallCardShadow:
+      "rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px",
     innerShadow: "rgba(0, 0, 0, 0.4) 0px -30px 30px -20px inset",
   },
 };
