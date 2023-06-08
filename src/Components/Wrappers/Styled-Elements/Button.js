1122d04394b959e8edf176d45e003d710630d164
berkaysson <berkaysonel85@gmail.com>
2023-06-08 22:18:08 +0300

    Refactor the style of Button

--- src/Components/Wrappers/Styled-Elements/Button.js
+++ src/Components/Wrappers/Styled-Elements/Button.js
@@ -2,12 +2,24 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  padding: 10px;
-  background-color: ${(props) =>
-    props.styeType === "primary" ? "blue" : "gray"};
-  color: white;
-  border: none;
+  background-color: ${({theme}) => theme.colors.secondary};
+  color: ${({theme}) => theme.colors.primary};
+  border: 1px solid ${({theme}) => theme.colors.alternative};
+  border-radius: ${({theme}) => theme.radius.large};
+  font-size: ${({theme}) => theme.sizes.medium};
+  padding: ${({theme}) => theme.sizes.small} ${({theme}) => theme.sizes.medium};
+  text-align: center;
   cursor: pointer;
+  box-shadow: ${({theme}) => theme.boxShadows.smallCardShadow};
+
+  &:hover{
+    font-weight: bold;
+  }
+
+  &:active{
+    border-color: ${({theme}) => theme.colors.primary};
+    background-color:${({theme}) => theme.colors.theme};
+  }
 `;
 
 const Button = ({ onClick, text, type, styeType }) => {
