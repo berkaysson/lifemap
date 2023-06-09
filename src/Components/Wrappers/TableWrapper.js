fac59026add86794302cdd442f757e6f17183476
berkaysson <berkaysonel85@gmail.com>
2023-06-09 20:25:46 +0300

    Styling the component

--- src/Components/Wrappers/TableWrapper.js
+++ src/Components/Wrappers/TableWrapper.js
@@ -1,5 +1,20 @@
 import { DataGrid } from "@mui/x-data-grid";
 import { theme } from "../../Style/theme";
+import styled from "styled-components";
+
+const TableWrapperWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: ${({theme}) => theme.sizes.small};
+  height: auto;
+  width: 273px; // column widths + bordersize + paddings
+  padding: ${({theme}) => theme.sizes.small};
+  border: 1px solid ${({theme}) => theme.colors.alternative};
+  border-radius: ${({theme}) => theme.radius.small};
+  box-shadow: ${({theme}) => theme.boxShadows.smallCardShadow};
+
+`
 
 const TableWrapper = ({ data }) => {
   const columns = [
@@ -14,7 +29,7 @@ const TableWrapper = ({ data }) => {
   }));
 
   return (
-    <div style={{ height: "auto", width: "255px" }}>
+    <TableWrapperWrapper>
       <h3>{data.categoryName} Activities</h3>
       <DataGrid
         rows={rows}
@@ -31,7 +46,6 @@ const TableWrapper = ({ data }) => {
           color: `${theme.colors.primary}`,
           borderColor: `${theme.colors.alternative}`,
           borderRadius: `${theme.radius.medium}`,
-          p: "1px",
 
           "& .MuiDataGrid-footerContainer": {
             display: "none",
@@ -50,7 +64,7 @@ const TableWrapper = ({ data }) => {
           },
         }}
       />
-    </div>
+    </TableWrapperWrapper>
   );
 };
 
