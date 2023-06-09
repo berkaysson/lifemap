996dd011756e62a453f4886ea04dd596f731319a
berkaysson <berkaysonel85@gmail.com>
2023-06-09 20:18:09 +0300

    Implement TableWrapper

--- src/Components/DataViewer/DataViewer.js
+++ src/Components/DataViewer/DataViewer.js
@@ -1,51 +1,24 @@
+import TableWrapper from "../Wrappers/TableWrapper";
+
 const DataViewer = ({ selectedDateDataUnit, activityCategories }) => {
   if (!selectedDateDataUnit || !activityCategories) return <p>No Data</p>;
 
-  const categoryHeaders = activityCategories.map((category) => (
-    <th
-      key={`${category.name}-header`}
-      colSpan={category.subCategories.length}
-    >
-      {category.name}
-    </th>
-  ));
-
-  const subCategoryHeaders = activityCategories.map((category) =>
-    category.subCategories.map((subCategory) => (
-      <th key={`${category.name}-${subCategory}`}>
-        {subCategory}
-      </th>
-    ))
-  );
-
-  let tableRow = [];
-
-  activityCategories.forEach((category) => {
-    category["subCategories"].forEach((subCategory) => {
-      let tableValue = selectedDateDataUnit[category.name]?.[subCategory] || 0;
-      tableRow.push(tableValue);
-    });
-  });
-
-  const subCategoryValues = tableRow.map((column, index) => (
-    <td key={`cell-${index}`}>{column}</td>
+  const tableDatas = activityCategories.map((category) => (
+    {
+      categoryName:category.name,
+      subCategories:category.subCategories,
+      subCategoryValues:category.subCategories.map((subCategory) => {
+        let tableValue = selectedDateDataUnit[category.name]?.[subCategory] || 0;
+        return tableValue;
+      })
+    }
   ));
 
   return (
-    <>
+    <div>
       <h3>{selectedDateDataUnit.date}</h3>
-      <table>
-        <thead>
-          {categoryHeaders}
-          <tr>{subCategoryHeaders}</tr>
-        </thead>
-        <tbody>
-          <tr>
-            {subCategoryValues}
-          </tr>
-        </tbody>
-      </table>
-    </>
+      {tableDatas.map((data, index) => (<TableWrapper data={data} key={index} />))}
+    </div>
   );
 };
 
