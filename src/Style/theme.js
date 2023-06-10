0ccf1ed1d628c4ff5a8f5953fabaaf9f599de4ae
berkaysson <berkaysonel85@gmail.com>
2023-06-10 17:14:41 +0300

    Add danger, success colors and innerSmallShadow box shadow props

--- src/Style/theme.js
+++ src/Style/theme.js
@@ -6,6 +6,8 @@ export const theme = {
     secondary: "#FFFFFF",
     alternative: "#d3d3d3",
     theme: themeColors,
+    danger: "#ff758f",
+    success: "#ccff33",
   },
   sizes: {
     small: "8px",
@@ -23,5 +25,6 @@ export const theme = {
     smallCardShadow:
       "rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px",
     innerShadow: "rgba(0, 0, 0, 0.4) 0px -30px 30px -20px inset",
+    innerSmallShadow: "rgba(0, 0, 0, 0.3) 0px -20px 20px -15px inset",
   },
 };
