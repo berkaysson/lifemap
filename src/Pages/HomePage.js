4460abdd19db7581264fcb987d1b77558ec6f59e
berkaysson <berkaysonel85@gmail.com>
2023-06-10 19:05:59 +0300

    Implement HeaderContent and ParagraphContent, and create layout for HomePage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,27 +1,71 @@
 import styled from "styled-components";
 
 import DataViewer from "../Components/DataViewer/DataViewer.js";
+import HeaderContent from "../Components/Contents/HeaderContent.js";
+import ParagraphContent from "../Components/Contents/ParagraphContent.js";
 
 const HomeWrapper = styled.section`
-  border: 2px solid red;
-  padding: 1rem;
+  display: grid;
+  grid-template-areas:
+    "header header header"
+    "welcome welcome info"
+    "tasks tasks tasks"
+    "viewer viewer reminder";
+  grid-template-rows: auto auto 1fr 1fr;
+  grid-template-columns: auto;
+  grid-gap: 10px;
 `;
 
-const CURRENT_DATE = new Date().toISOString().slice(0, 10);
+const Header = styled.header`
+  grid-area: header;
+`;
+
+const Welcome = styled.div`
+  grid-area: welcome;
+`;
+
+const Viewer = styled(DataViewer)`
+  border: 1px black solid;
+  grid-area: viewer;
+`;
+
+const Info = styled.div`
+  border: 1px black solid;
+  grid-area: info;
+`;
+
+const Tasks = styled.div`
+  border: 1px black solid;
+  grid-area: tasks;
+`;
+
+const Reminder = styled.div`
+  border: 1px black solid;
+  grid-area: reminder;
+`;
 
 const HomePage = ({ activityCategories, todaysActivityDataUnit }) => {
   return (
     <HomeWrapper>
-      <h1>HOME</h1>
-      <div>Welcome USER_NAME</div>
-      <div>{CURRENT_DATE}</div>
-      <DataViewer
+      <Header>
+        <HeaderContent headerText="HOME" />
+      </Header>
+      <Welcome>
+        <ParagraphContent
+          text={
+            "Welcome to Lifemap, your personal life tracking tool. Stay organized and achieve your goals by tracking your activities and finances in one place. Take control of your life and make every day count."
+          }
+        />
+      </Welcome>
+      <Viewer
         selectedDateDataUnit={todaysActivityDataUnit}
         activityCategories={activityCategories}
       />
-      <div>A COMPONENT TO VIEW TO ACTIVE TASKS-HABITS</div>
-      <div>A REMINDER COMPONENT TO TASKS-HABITS WHICH TIME EXPIRES</div>
-      <div>SETTINGS AND INFO LINK</div>
+      <Info>INFO</Info>
+      <Tasks>A COMPONENT TO VIEW ACTIVE TASKS-HABITS</Tasks>
+      <Reminder>
+        A REMINDER COMPONENT FOR TASKS-HABITS WHICH TIME EXPIRES
+      </Reminder>
     </HomeWrapper>
   );
 };
