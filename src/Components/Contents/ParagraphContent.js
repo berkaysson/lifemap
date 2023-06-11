b149ab84f178787c328d435810a7caa0af80afc4
berkaysson <berkaysonel85@gmail.com>
2023-06-11 03:02:47 +0300

    Edit border radius style

--- src/Components/Contents/ParagraphContent.js
+++ src/Components/Contents/ParagraphContent.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   border: 1px solid ${({theme})=> theme.colors.alternative};
   padding: ${({theme})=> theme.sizes.medium};
-  border-radius: ${({theme})=> theme.radius.small};
+  border-radius: ${({theme})=> theme.radius.medium};
   box-shadow: ${({theme})=> theme.boxShadows.smallCardShadow};
 `;
 
