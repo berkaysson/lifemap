f032fcfb522b12fb68104933032cf535eb5d5fd0
berkaysson <berkaysonel85@gmail.com>
2023-06-13 01:51:21 +0300

    Add main styles and layout

--- src/Pages/FinancePage.js
+++ src/Pages/FinancePage.js
@@ -2,10 +2,66 @@ import styled from "styled-components";
 
 import FinancesForm from "../Components/Finances/FinancesForm";
 import FinanceDataList from "../Components/Finances/FinanceDataList";
+import ParagraphContent from "../Components/Contents/ParagraphContent";
+import HeaderContent from "../Components/Contents/HeaderContent";
 
 const Wrapper = styled.section`
-  border: 2px solid aqua;
-  padding: 1rem;
+  display: grid;
+  grid-template-columns: 1fr minmax(550px, 80%) 1fr;
+  grid-template-rows: repeat(4, auto);
+  align-content: start;
+  justify-content: center;
+  align-items: stretch;
+  justify-items: stretch;
+  gap: ${({ theme }) => theme.sizes.large};
+  width: 100%;
+  height: 100%;
+`;
+
+const Header = styled.header`
+  grid-area: 1 / 1 / 2 / 4;
+`;
+
+const Welcome = styled.div`
+  grid-area: 2 / 2 / 3 / 3;
+`;
+
+const Form = styled.div`
+  grid-area: 3 / 2 / 4 / 3;
+  display: grid;
+  align-content: start;
+  justify-content: center;
+  align-items: stretch;
+  justify-items: stretch;
+  padding: ${({ theme }) => theme.sizes.medium};
+  gap: 1px;
+  box-shadow: ${({ theme }) => theme.boxShadows.smallCardShadow};
+  border: 1px solid ${({ theme }) => theme.colors.alternative};
+  border-radius: ${({ theme }) => theme.radius.medium};
+
+`;
+
+const Finance = styled.div`
+  display: grid;
+  grid-template-rows: 1fr; //change to 1fr 1fr responsive design
+  grid-template-columns: 70% auto; //change to 1fr responsive design
+`;
+
+const List = styled.div`
+  grid-area: 4 / 2 / 5 / 3;
+  align-content: start;
+  justify-content: center;
+  align-items: stretch;
+  justify-items: stretch;
+  padding: ${({ theme }) => theme.sizes.medium};
+  box-shadow: ${({ theme }) => theme.boxShadows.smallCardShadow};
+  border: 1px solid ${({ theme }) => theme.colors.alternative};
+  border-radius: ${({ theme }) => theme.radius.medium};
+`;
+
+const FormHeader = styled.h2`
+  text-align: center;
+  margin-bottom: ${({ theme }) => theme.sizes.small};
 `;
 
 const FinancePage = ({
@@ -17,23 +73,34 @@ const FinancePage = ({
 }) => {
   return (
     <Wrapper>
-      <h1>FinancePage</h1>
-      <FinancesForm
-        expenseCategory={financialCategories.find(
-          (obj) => obj.id === "expenseCategories"
-        )}
-        incomeCategory={financialCategories.find(
-          (obj) => obj.id === "incomeCategories"
-        )}
-        onAddFinancialDataUnit={onAddFinancialDataUnit}
-      />
-      <FinanceDataList
-        financeDataUnits={financeDataUnits}
-        onDeleteFinancialDataUnit={onDeleteFinancialDataUnit}
-        onUpdateFinancialDataUnit={onUpdateFinancialDataUnit}
-      />
-      <div>A TEMPLATE FOR EXPENSES, LIKE WAGE = 10000</div>
-      <div>A COMPONENT TO EDIT EXPENSE CATEGORIES</div>
+      <Header>
+        <HeaderContent headerText={"Finances"} />
+      </Header>
+      <Welcome>
+        <ParagraphContent>Definition to page</ParagraphContent>
+      </Welcome>
+      <Form>
+      <FormHeader>Finance Form</FormHeader>
+      <Finance>
+        <FinancesForm
+          expenseCategory={financialCategories.find(
+            (obj) => obj.id === "expenseCategories"
+          )}
+          incomeCategory={financialCategories.find(
+            (obj) => obj.id === "incomeCategories"
+          )}
+          onAddFinancialDataUnit={onAddFinancialDataUnit}
+        />
+        <ParagraphContent>Definition to form</ParagraphContent>
+        </Finance>
+      </Form>
+      <List>
+        <FinanceDataList
+          financeDataUnits={financeDataUnits}
+          onDeleteFinancialDataUnit={onDeleteFinancialDataUnit}
+          onUpdateFinancialDataUnit={onUpdateFinancialDataUnit}
+        />
+      </List>
     </Wrapper>
   );
 };
