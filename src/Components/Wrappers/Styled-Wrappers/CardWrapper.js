09fc70ba682da41e4b9b603aa3bc9978c8329aa3
berkaysson <berkaysonel85@gmail.com>
2023-06-13 20:55:48 +0300

    Implement AnimatedCards

--- src/Components/Wrappers/Styled-Wrappers/CardWrapper.js
+++ src/Components/Wrappers/Styled-Wrappers/CardWrapper.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { AnimatedCards } from "../AnimatedCards";
 
 const CardWrapperWrapper = styled.div`
   border: 1px solid ${({ theme }) => theme.colors.alternative};
@@ -10,11 +10,10 @@ const CardWrapperWrapper = styled.div`
 
 export const CardWrapper = ({ children }) => {
   return (
-    <motion.div
-      animate={{ opacity: [0,1], transform:["scale(0.9)","scale(1)"]}}
+    <AnimatedCards
       transition={{duration:0.3,ease: "easeOut"}}
     >
       <CardWrapperWrapper>{children}</CardWrapperWrapper>
-    </motion.div>
+    </AnimatedCards>
   );
 };
