68922d4f2b32e2dce5f3c22ba27a20cf0771c442
berkaysson <berkaysonel85@gmail.com>
2023-06-13 20:38:44 +0300

    Add framer-motion animation to CardWrapper

--- src/Components/Wrappers/Styled-Wrappers/CardWrapper.js
+++ src/Components/Wrappers/Styled-Wrappers/CardWrapper.js
@@ -1,8 +1,20 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-export const CardWrapper = styled.div`
+const CardWrapperWrapper = styled.div`
   border: 1px solid ${({ theme }) => theme.colors.alternative};
   border-radius: ${({ theme }) => theme.radius.medium};
   box-shadow: ${({ theme, disableBoxShadow = false }) =>
     disableBoxShadow ? "none" : theme.boxShadows.smallCardShadow};
 `;
+
+export const CardWrapper = ({ children }) => {
+  return (
+    <motion.div
+      animate={{ opacity: [0,1], transform:["scale(0.9)","scale(1)"]}}
+      transition={{duration:0.3,ease: "easeOut"}}
+    >
+      <CardWrapperWrapper>{children}</CardWrapperWrapper>
+    </motion.div>
+  );
+};
