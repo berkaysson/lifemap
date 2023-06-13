34fc5faa33aab5efab4744b8751aeba43bc7cb6a
berkaysson <berkaysonel85@gmail.com>
2023-06-13 20:55:54 +0300

    Update animations

--- src/Style/animations.js
+++ src/Style/animations.js
@@ -1,11 +1,14 @@
 export const animations = {
-  pageAnimation : {
-    initial:{opacity:0,},
-    animate:{opacity:1,},
-    exit:{opacity:0,},
+  pageAnimation: {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
   },
   cardAnimation: {
-    animate: {opacity: [0,1], transform:["scale(0.9)","scale(1)"]},
-    exit:{opacity:0,},
-  }
-}
+    animate: {
+      opacity: [0, 0.2, 0.2, 0.3, 0.5, 1],
+      transform: ["translateX(-15%)", "translateX(0)"],
+    },
+    exit: { opacity: 0 },
+  },
+};
