89fb1485401c77156fa2be5134d61d9cdb7ea74c
berkaysson <berkaysonel85@gmail.com>
2023-06-14 17:00:38 +0300

    Create FilteredTasksHabitsList to filter all tasks and habits if they are still not closed and not fulfilled (still active) and list them

---
+++ src/Components/Tasks&Habits/FilteredTasksHabitsList.js
@@ -0,0 +1,88 @@
+import { CardWrapper } from "../Wrappers/Styled-Wrappers/CardWrapper";
+import TaskItem from "./Tasks/TaskItem";
+import HabitItem from "./Habits/HabitItem";
+
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: ${({ theme }) => theme.sizes.medium};
+  width: 100%;
+  padding: ${({ theme }) => theme.sizes.medium};
+`;
+
+const Container = styled.div`
+  border: 1px solid ${({ theme }) => theme.colors.alternative};
+  border-radius: ${({ theme }) => theme.radius.medium};
+  padding: ${({ theme }) => theme.sizes.medium};
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.sizes.medium};
+`;
+
+const ListWrapper = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.sizes.medium};
+`;
+
+const FilteredTasksHabitsList = ({ taskDataUnits, habitDataUnits }) => {
+  return (
+    <CardWrapper>
+      <Wrapper>
+        <Container>
+          <h2>Active Tasks</h2>
+          <div>
+            {taskDataUnits.length > 0 ? (
+              <ListWrapper>
+                {taskDataUnits.map((task) =>
+                  !task.isFulfilled && !task.isClosed ? (
+                    <TaskItem
+                      task={task}
+                      key={task.id}
+                      isDeleteActive={false}
+                      onDeleteTaskDataUnit={() => {}}
+                    />
+                  ) : (
+                    ""
+                  )
+                )}
+              </ListWrapper>
+            ) : (
+              <p>No tasks found.</p>
+            )}
+          </div>
+        </Container>
+        <Container>
+          <h2>Active Habits</h2>
+          <div>
+            {habitDataUnits.length > 0 ? (
+              <ListWrapper>
+                {habitDataUnits.map((habit) =>
+                  !habit.isFulfilled && !habit.isClosed ? (
+                    <HabitItem
+                      habit={habit}
+                      key={habit.id}
+                      isDeleteActive={false}
+                      onDeleteHabitDataUnit={() => {}}
+                    />
+                  ) : (
+                    ""
+                  )
+                )}
+              </ListWrapper>
+            ) : (
+              <p>No tasks found.</p>
+            )}
+          </div>
+        </Container>
+      </Wrapper>
+    </CardWrapper>
+  );
+};
+
+export default FilteredTasksHabitsList;
