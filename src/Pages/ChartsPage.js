7a0ee22faeaac8a27327215cd2daef7937bb612c
berkaysson <berkaysonel85@gmail.com>
2023-06-14 17:14:55 +0300

    Refactor ChartsPage

--- src/Pages/ChartsPage.js
+++ src/Pages/ChartsPage.js
@@ -1,17 +1,48 @@
 import styled from "styled-components";
 import { AnimatedPage } from "../Components/Wrappers/AnimatedPage";
+import HeaderContent from "../Components/Contents/HeaderContent";
 
 const Wrapper = styled.section`
-  border: 2px solid brown;
-  padding: 1rem;
+  display: grid;
+  grid-template-columns: 1fr minmax(550px, 80%) 1fr;
+  grid-template-rows: repeat(3, auto);
+  align-content: start;
+  justify-content: center;
+  align-items: stretch;
+  justify-items: stretch;
+  gap: ${({ theme }) => theme.sizes.large};
+  width: 100%;
+  height: 100%;
+`;
+
+const Header = styled.header`
+  grid-area: 1 / 1 / 2 / 4;
+`;
+
+const Content = styled.div`
+  grid-area: 2 / 2 / 4 / 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 3rem;
+  width: 100%;
+  border: 1px solid ${({ theme }) => theme.colors.alternative};
+  border-radius: ${({ theme }) => theme.radius.medium};
+  padding: ${({ theme }) => theme.sizes.large};
 `;
 
 const ChartsPage = () => {
   return (
     <AnimatedPage>
       <Wrapper>
-        <h1>ChartsPage</h1>
-        <div>CHARTS</div>
+        <Header>
+          <HeaderContent headerText={"Charts"} />
+        </Header>
+        <Content>
+          <h3>UNDER CONSTRUCTION</h3>
+          <h4>Coming soon...</h4>
+        </Content>
       </Wrapper>
     </AnimatedPage>
   );
