947e8a5f73f2157641f30600541d601eb1b7ed91
berkaysson <berkaysonel85@gmail.com>
2023-06-14 18:35:42 +0300

    Update to Link of lifemap

--- src/Components/Contents/HeaderContent.js
+++ src/Components/Contents/HeaderContent.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { Link } from "react-router-dom";
+
 const HeaderContentWrapper = styled.div`
   display: flex;
   flex-direction: row;
@@ -9,11 +11,22 @@ const HeaderContentWrapper = styled.div`
   font-size: 1.25em;
 `;
 
+const LinkWrapper = styled(Link)`
+  color: ${({theme}) => theme.colors.theme};
+  font-weight: bold;
+  font-size: 1.1em;
+
+  &>span{
+    color: ${({theme}) => theme.colors.themeSecondary};
+  }
+`;
+
+
 const HeaderContent = ({ headerText }) => {
   return (
     <HeaderContentWrapper>
       <h1>{headerText}</h1>
-      <a href="/">lifemap</a>
+      <LinkWrapper to="/">life<span>map</span></LinkWrapper>
     </HeaderContentWrapper>
   );
 };
