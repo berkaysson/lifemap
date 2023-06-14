28c6f4d556fd2c5e7721097e4d9f807d91ca6dba
berkaysson <berkaysonel85@gmail.com>
2023-06-14 17:21:56 +0300

    Style ImportExport file

--- src/Components/Others/ImportExport.js
+++ src/Components/Others/ImportExport.js
@@ -1,11 +1,27 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../Wrappers/Styled-Elements/Button";
+import { CardWrapper } from "../Wrappers/Styled-Wrappers/CardWrapper";
 
-import styled from "styled-components";
-import StyledInput from "../Wrappers/Styled-Elements/StyledInput";
 
-const DexieImportExportWrapper = styled.div``;
+
+const DexieImportExportWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: ${({theme}) => theme.sizes.medium};
+  gap: ${({ theme }) => theme.sizes.large};
+  
+  &>div{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    gap: ${({ theme }) => theme.sizes.small};
+  }
+`;
 
 const ImportExport = ({ onExport, onImport }) => {
   const [file, setFile] = useState(null);
@@ -29,13 +45,19 @@ const ImportExport = ({ onExport, onImport }) => {
   };
 
   return (
-    <DexieImportExportWrapper>
-      <Button text={"Export"} type={"button"} onClick={onExport} />
-
-      <h4>Import Database</h4>
-      <input type="file" accept=".json" onChange={handleFileChange} />
-      <Button text={"Import"} type={"button"} onClick={handleImport} />
-    </DexieImportExportWrapper>
+    <CardWrapper>
+      <DexieImportExportWrapper>
+        <div>
+          <h4>Export Database</h4>
+          <Button text={"Export"} type={"button"} onClick={onExport} />
+        </div>
+        <div>
+          <h4>Import Database</h4>
+          <input type="file" accept=".json" onChange={handleFileChange} />
+          <Button text={"Import"} type={"button"} onClick={handleImport} />
+        </div>
+      </DexieImportExportWrapper>
+    </CardWrapper>
   );
 };
 
