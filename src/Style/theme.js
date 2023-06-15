2e4273c7e4446029a6e6bc91f8f97398d464b39a
berkaysson <berkaysonel85@gmail.com>
2023-06-15 22:42:19 +0300

    increase shadows intensity

--- src/Style/theme.js
+++ src/Style/theme.js
@@ -22,10 +22,10 @@ export const theme = {
   },
   boxShadows: {
     themeShadow: `0 0 6px 0px ${themeColors}`,
-    largeCardShadow: "rgba(0, 0, 0, 0.5) 0px 20px 20px 5px",
+    largeCardShadow: "rgba(0, 0, 0, 0.6) 0px 20px 20px 5px",
     smallCardShadow:
-      "rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px",
-    innerShadow: "rgba(171, 196, 255, 0.4) 0px -30px 30px -20px inset",
+      "rgba(0, 0, 0, 0.09) 0px 1px 2px, rgba(0, 0, 0, 0.09) 0px 2px 4px, rgba(0, 0, 0, 0.09) 0px 4px 8px, rgba(0, 0, 0, 0.09) 0px 8px 16px, rgba(0, 0, 0, 0.09) 0px 16px 32px",
+    innerShadow: "rgba(0, 0, 0, 0.5) 0px -30px 30px -20px inset",
     innerSmallShadow: "rgba(171, 196, 255, 0.4) 0px -20px 20px -15px inset",
   },
 };
