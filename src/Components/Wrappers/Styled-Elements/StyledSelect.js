bf7357d0415eafb9123ef115b664c19bd0180a6b
berkaysson <berkaysonel85@gmail.com>
2023-06-17 22:46:01 +0300

    Remove react-select from StyledSelect and recreate it with material UI Select

--- src/Components/Wrappers/Styled-Elements/StyledSelect.js
+++ src/Components/Wrappers/Styled-Elements/StyledSelect.js
@@ -1,31 +1,64 @@
-import Select from "react-select";
+import React from "react";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { theme as myTheme } from "../../../Style/theme";
 import styled from "styled-components";
 
-import { theme as myTheme } from "../../../Style/theme";
-
-const StyledSelectWrapper = styled(Select)`
+const StyledSelectWrapper = styled.div`
   box-shadow: ${myTheme.boxShadows.smallCardShadow};
   border-radius: ${myTheme.radius.small};
   font-weight: bold;
   max-width: 300px;
   min-width: 250px;
   transition: background-color 0.4s, color 0.4s;
-`;
+`
 
 export const StyledSelect = (props) => {
+  const handleChange = (event) => {
+    props.onChange(event.target.value)
+  }
+
   return (
-    <StyledSelectWrapper
-      {...props}
-      theme={(theme) => ({
-        ...theme,
-        borderRadius: `${myTheme.radius.small}`,
-        colors: {
-          ...theme.colors,
-          primary25: `${myTheme.colors.alternative}`,
-          primary50: `${myTheme.colors.theme}`,
-          primary: `${myTheme.colors.theme}`,
-        },
-      })}
-    />
+    <StyledSelectWrapper>
+    <FormControl fullWidth>
+      <InputLabel id="select-label">{props.placeholder}</InputLabel>
+      <Select
+        labelId="select-label"
+        onChange={handleChange}
+        value={props.value ? props.value : ""}
+        label={props.placeholder}
+        variant="outlined"
+        sx={{
+          "& .MuiSelect-outlined":{
+            color: myTheme.colors.primary,
+          },
+          "& .MuiInputLabel-root": {
+            color: myTheme.colors.primary,
+          },
+          '& .MuiOutlinedInput-notchedOutline': {
+            borderColor: myTheme.colors.alternative
+          },
+          "& .Mui-focused":{
+            borderColor:"red"
+          }
+        }}
+      >
+        {props.options.map((option) => (
+          <MenuItem 
+          value={option} 
+          disableRipple
+          sx={{
+            "&:hover":{
+              fontWeight:"bold",
+            },
+
+            "&:focus":{
+              bgcolor: myTheme.colors.theme,
+            }
+          }}
+          >{option.label}</MenuItem>
+        ))}
+      </Select>
+      </FormControl>
+    </StyledSelectWrapper>
   );
 };
