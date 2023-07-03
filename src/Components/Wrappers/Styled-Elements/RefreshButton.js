fafd310b1a9b048418179448b344ead103b98602
berkaysson <berkaysonel85@gmail.com>
2023-07-03 21:50:01 +0300

    Create a RefreshButton to fetch all data by click event

---
+++ src/Components/Wrappers/Styled-Elements/RefreshButton.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import RefreshIcon from '@mui/icons-material/Refresh';
+
+const Wrapper = styled.i`
+  position: absolute;
+  top: 40%;
+  right: 1rem;
+  cursor: pointer;
+  display: inline-flex;
+  border: 1px solid ${({theme})=> theme.colors.alternative};
+  border-radius: ${({theme})=> theme.radius.large};
+  padding: 5px;
+  background-color: ${({theme})=> theme.colors.secomdary};
+
+  &:hover {
+    background-color: ${({theme})=> theme.colors.alternative};
+  }
+
+  &:active {
+    border-color: ${({theme})=> theme.colors.theme};
+  }
+`
+
+const RefreshButton = ( {fetchUpdateHandler} ) => {
+  return <Wrapper onClick={fetchUpdateHandler}>
+    <RefreshIcon />
+  </Wrapper>
+}
+
+export default RefreshButton;
