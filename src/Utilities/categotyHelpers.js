26b5cd40b9d15b394a1148c65e0c41886eaf023a
berkaysson <berkaysonel85@gmail.com>
2023-07-11 22:14:12 +0300

    Refactor methods to imrove performance and readability

--- src/Utilities/categotyHelpers.js
+++ src/Utilities/categotyHelpers.js
@@ -1,19 +1,28 @@
-export const updateCategoryHelper = async (db, category, subCategory, snackBarHandler) => {
+export const updateCategoryHelper = async (
+  db,
+  category,
+  subCategory,
+  snackBarHandler
+) => {
   try {
     const categoryData = await db.categoriesData.get(category);
-    if (categoryData) {
-      if (!categoryData.hasOwnProperty("subCategories")) {
-        categoryData.subCategories = [subCategory];
-      } else {
-        if (categoryData.subCategories.includes(subCategory)) {
-          snackBarHandler(`${subCategory} already exists in ${category} 😕`, "warning");
-          return;
-        }
-        categoryData.subCategories.push(subCategory);
-      }
-      await db.categoriesData.put(categoryData);
-      snackBarHandler("Category Successfully Created 🥳", "success");
+    if (!categoryData) {
+      return;
     }
+
+    if (!categoryData.hasOwnProperty("subCategories")) {
+      categoryData.subCategories = [subCategory];
+    } else {
+      if (categoryData.subCategories.includes(subCategory)) {
+        return snackBarHandler(
+          `${subCategory} already exists in ${category} 😕`,
+          "warning"
+        );
+      }
+      categoryData.subCategories.push(subCategory);
+    }
+    await db.categoriesData.put(categoryData);
+    snackBarHandler("Category Successfully Created 🥳", "success");
   } catch (error) {
     console.error("Error updating category data:", error);
     snackBarHandler("Category Could not be Created 😢", "error");
@@ -28,25 +37,41 @@ export const deleteSubCategoryHelper = async (
   allActivityDataUnits,
   snackBarHandler
 ) => {
+  if (checkSubCategoryUsed(allActivityDataUnits, categoryName, subCategory)) {
+    return snackBarHandler(
+      `${subCategory}, This category has been used before, can not be deleted 🤨`,
+      "warning"
+    );
+  }
+
+  try {
+    const categoryData = await db.categoriesData.get(categoryID);
+    if (!categoryData) {
+      return;
+    }
+
+    const subCategories = categoryData.subCategories.filter(
+      (s) => s !== subCategory
+    );
+    await db.categoriesData.update(categoryID, { subCategories });
+    snackBarHandler("Category Successfully Deleted 👍", "success");
+  } catch (error) {
+    console.error("Error deleting subcategory:", error);
+    snackBarHandler("Category Could not be Deleted 😧", "error");
+  }
+};
+
+const checkSubCategoryUsed = (
+  allActivityDataUnits,
+  categoryName,
+  subCategory
+) => {
   if (
     allActivityDataUnits.some(
       (activityDataUnit) => activityDataUnit[categoryName]?.[subCategory] > 0
     )
   ) {
-    return snackBarHandler(`${subCategory}, This category has been used before, can not be deleted 🤨`, "warning");
+    return true;
   }
-
-  try {
-    const categoryData = await db.categoriesData.get(categoryID);
-    if (categoryData) {
-      const subCategories = categoryData.subCategories.filter(
-        (s) => s !== subCategory
-      );
-      await db.categoriesData.update(categoryID, { subCategories });
-      snackBarHandler("Category Successfully Deleted 👍", "success");
-    }
-  } catch (error) {
-    console.error("Error deleting subcategory:", error);
-    snackBarHandler("Category Could not be Deleted 😧", "error");
-  }
+  return false;
 };
