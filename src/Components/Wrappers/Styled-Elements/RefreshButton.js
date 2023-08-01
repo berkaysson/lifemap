02e7a7e1fd147a9cc2f6a94ece6290612dfd0da1
berkaysson <berkaysonel85@gmail.com>
2023-08-01 15:07:08 +0300

    Add box-shadow and change border color of RefreshButton to make it more noticeable

--- src/Components/Wrappers/Styled-Elements/RefreshButton.js
+++ src/Components/Wrappers/Styled-Elements/RefreshButton.js
@@ -4,11 +4,12 @@ import RefreshIcon from '@mui/icons-material/Refresh';
 const Wrapper = styled.i`
   cursor: pointer;
   display: inline-flex;
-  border: 1px solid ${({theme})=> theme.colors.alternative};
+  border: 1px solid ${({theme})=> theme.colors.secondary};
   border-radius: ${({theme})=> theme.radius.large};
   padding: 5px;
   background-color: ${({theme})=> theme.colors.secondary};
   transition: 0.2s transform ease;
+  box-shadow: ${({theme})=> theme.boxShadows.innerSmallShadow};
 
   &:hover {
     background-color: ${({theme})=> theme.colors.alternative};
