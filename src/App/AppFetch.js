f705b7b7b646c72fbf1bbf9dc61eeb732f569a27
berkaysson <berkaysonel85@gmail.com>
2023-08-12 17:59:20 +0300

    Remove performanceState and consoles

--- src/App/AppFetch.js
+++ src/App/AppFetch.js
@@ -9,29 +9,13 @@ import {
 } from "../Utilities/task&habitCheckHelpers";
 import LoadingModal from "../Components/Wrappers/LoadingModal";
 
-const samplePerformanceState = {
-  fetches: {
-    category: 0,
-    finance: 0,
-    tasks: 0,
-    habits: 0,
-    activities: 0,
-  },
-  checks: {
-    tasks: 0,
-    habits: 0,
-  },
-};
-
 const AppFetch = ({
   onGetAllCategories,
   onGetAllFinancialDataUnits,
   onGetAllActivityDataUnits,
-  onGetActivityDataUnit,
   onGetAllTaskDataUnits,
   onGetAllHabitDataUnits,
   contentProps,
-  onFetchUpdate,
   isNeedFetchUpdate,
   onEditTaskDataUnitFulfilled,
   onEditTaskDataUnitClosed,
@@ -51,25 +35,10 @@ const AppFetch = ({
   const [habitDataUnits, setHabitDataUnits] = useState([]);
   const [todaysActivityDataUnit, setTodaysActivityDataUnit] = useState(null);
 
-  const [isLoading, setIsLoading] = useState(true); // Loading state
-
-  const [performanceState, setPerformanceState] = useState({
-    fetches: {
-      category: 0,
-      finance: 0,
-      tasks: 0,
-      habits: 0,
-      activities: 0,
-    },
-    checks: {
-      tasks: 0,
-      habits: 0,
-    },
-  });
+  const [isLoading, setIsLoading] = useState(true);
 
   async function fetchCategories() {
     try {
-      const startTime = performance.now();
       const newCategories = await onGetAllCategories();
       setCategories(newCategories);
       setActivityCategories(() =>
@@ -84,15 +53,7 @@ const AppFetch = ({
             item.id === "expenseCategories" || item.id === "incomeCategories"
         )
       );
-      const endTime = performance.now();
-      setPerformanceState((prevState) => ({
-        ...prevState,
-        fetches: {
-          ...prevState.fetches,
-          category: prevState.fetches.category + endTime - startTime,
-        },
-      }));
-      console.log("Categories fetched successfully");
+      // console.log("Categories fetched successfully");
     } catch (error) {
       console.error("Error fetching categories:", error);
     }
@@ -100,18 +61,9 @@ const AppFetch = ({
 
   async function fetchFinanceDataUnits() {
     try {
-      const startTime = performance.now();
       const allFinanceDataUnits = await onGetAllFinancialDataUnits();
       setFinanceDatas(allFinanceDataUnits);
-      const endTime = performance.now();
-      setPerformanceState((prevState) => ({
-        ...prevState,
-        fetches: {
-          ...prevState.fetches,
-          finance: prevState.fetches.finance + endTime - startTime,
-        },
-      }));
-      console.log("Finance data fetched successfully");
+      // console.log("Finance data fetched successfully");
     } catch (error) {
       console.error("Error fetching finance data:", error);
     }
@@ -119,7 +71,6 @@ const AppFetch = ({
 
   async function fetchActivityDataUnits() {
     try {
-      const startTime = performance.now();
       const allActivityDataUnits = await onGetAllActivityDataUnits();
       setActivityDatas(allActivityDataUnits);
       const newTodaysActivityDataUnit = allActivityDataUnits?.slice(-1)[0];
@@ -129,15 +80,7 @@ const AppFetch = ({
         activityDataUnitsMapSample.set(dataUnit.date, dataUnit);
       });
       setActivityDatasMap(activityDataUnitsMapSample);
-      const endTime = performance.now();
-      setPerformanceState((prevState) => ({
-        ...prevState,
-        fetches: {
-          ...prevState.fetches,
-          activities: prevState.fetches.activities + endTime - startTime,
-        },
-      }));
-      console.log("Activity data fetched successfully");
+      // console.log("Activity data fetched successfully");
     } catch (error) {
       console.error("Error fetching activity data:", error);
     }
@@ -145,18 +88,9 @@ const AppFetch = ({
 
   async function fetchTaskDataUnits() {
     try {
-      const startTime = performance.now();
       const allTaskDataUnits = await onGetAllTaskDataUnits();
       setTaskDataUnits(allTaskDataUnits);
-      const endTime = performance.now();
-      setPerformanceState((prevState) => ({
-        ...prevState,
-        fetches: {
-          ...prevState.fetches,
-          tasks: prevState.fetches.tasks + endTime - startTime,
-        },
-      }));
-      console.log("Task data fetched successfully");
+      // console.log("Task data fetched successfully");
     } catch (error) {
       console.error("Error fetching Task data:", error);
     }
@@ -164,25 +98,15 @@ const AppFetch = ({
 
   async function fetchHabitDataUnits() {
     try {
-      const startTime = performance.now();
       const allHabitDataUnits = await onGetAllHabitDataUnits();
       setHabitDataUnits(allHabitDataUnits);
-      const endTime = performance.now();
-      setPerformanceState((prevState) => ({
-        ...prevState,
-        fetches: {
-          ...prevState.fetches,
-          habits: prevState.fetches.habits + endTime - startTime,
-        },
-      }));
-      console.log("Habit data fetched successfully");
+      // console.log("Habit data fetched successfully");
     } catch (error) {
       console.error("Error fetching Habit data:", error);
     }
   }
 
   const checkTasks = async () => {
-    const startTime = performance.now();
     const allTaskDataUnits = taskDataUnits;
 
     if (allTaskDataUnits) {
@@ -200,18 +124,9 @@ const AppFetch = ({
         }
       }
     }
-    const endTime = performance.now();
-    setPerformanceState((prevState) => ({
-      ...prevState,
-      checks: {
-        ...prevState.checks,
-        tasks: prevState.checks.tasks + endTime - startTime,
-      },
-    }));
   };
 
   const checkHabits = async () => {
-    const startTime = performance.now();
     const allHabitDataUnits = habitDataUnits;
 
     if (allHabitDataUnits) {
@@ -223,14 +138,6 @@ const AppFetch = ({
         }
       }
     }
-    const endTime = performance.now();
-    setPerformanceState((prevState) => ({
-      ...prevState,
-      checks: {
-        ...prevState.checks,
-        habits: prevState.checks.habits + endTime - startTime,
-      },
-    }));
   };
 
   const checkCheckpointsOfHabitUnit = async (habitUnit) => {
@@ -285,25 +192,21 @@ const AppFetch = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [todaysActivityDataUnit, activityDataUnits]);
 
-  useEffect(()=>{
-    (async ()=>{
+  useEffect(() => {
+    (async () => {
       await checkTasks();
       setIsLoading(false);
     })();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, taskDataUnits);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [taskDataUnits]);
 
-  useEffect(()=>{
-    (async ()=>{
+  useEffect(() => {
+    (async () => {
       await checkHabits();
       setIsLoading(false);
     })();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, habitDataUnits);
-
-  useEffect(() => {
-    console.log(performanceState);
-  }, [performanceState]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [habitDataUnits]);
 
   const updatedContentProps = {
     ...contentProps,
@@ -317,56 +220,10 @@ const AppFetch = ({
     todaysActivityDataUnit,
   };
 
-  const impactPerformance = () => {
-    // Calculate the total for fetches
-    const totalFetches = Object.values(performanceState.fetches).reduce(
-      (sum, value) => sum + value,
-      0
-    );
-
-    // Calculate the total for checks
-    const totalChecks = Object.values(performanceState.checks).reduce(
-      (sum, value) => sum + value,
-      0
-    );
-
-    // Calculate the percentages for fetches
-    const fetchesPercentages = {};
-    for (const key in performanceState.fetches) {
-      fetchesPercentages[key] =
-        (performanceState.fetches[key] / (totalFetches + totalChecks)) * 100;
-    }
-
-    // Calculate the percentages for checks
-    const checksPercentages = {};
-    for (const key in performanceState.checks) {
-      checksPercentages[key] =
-        (performanceState.checks[key] / (totalChecks + totalFetches)) * 100;
-    }
-
-    // Return an object with calculated percentages
-    console.log({fetchesPercentages, checksPercentages, total:totalChecks+totalFetches});
-  };
-
   return (
     <>
       {isLoading ? <LoadingModal /> : ""}
-
       <AppContent {...updatedContentProps} />
-      <button
-        onClick={() => {
-          setPerformanceState(samplePerformanceState);
-        }}
-        style={{ position: "fixed", zIndex: "99999", top: "0", left: "0" }}
-      >
-        RESET PERFORMANCE TEST
-      </button>
-      <button
-        onClick={impactPerformance}
-        style={{ position: "fixed", zIndex: "99999", top: "0", left: "50%" }}
-      >
-        SHOW IMPACT
-      </button>
     </>
   );
 };
